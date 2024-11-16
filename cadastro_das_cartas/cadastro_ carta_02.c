954d49251a64528730b0726c0b50af2954863f0a
andrebrisson <andrebrissoon@gmail.com>
2024-11-16 21:37:46 +0000

    printf padrão implementação de char de país, estado

--- cadastro_das_cartas/cadastro_ carta_02.c
+++ cadastro_das_cartas/cadastro_ carta_02.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 
 int main (){
-char cidade [4] ="SP";
-char nome [20] = "São Paulo";
+char pais [20] = "Brasil";
+char estado [20] = "São Paulo";    
+char codgcidade [4] ="S01";
+char nome [20] = "São Bernado";
 int populacao = 1000000;
 float area = 10890.79;
 double pib = 50390.30;
 int turismo = 2;
 
-printf ("codigo da cidade: %s\n", cidade);
+printf ("país: %s\n", pais);
+printf ("estado: %s\n", estado);
+printf ("codigo da cidade: %s\n", codgcidade);
 printf ("nome: %s\n", nome);
 printf ("populacao da cidade: %d\n", populacao);
 printf ("area %.2f\n", area);
--- cadastro_das_cartas/cadastro_carta_03.c
+++ cadastro_das_cartas/cadastro_carta_03.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 
 int main (){
-char cidade [4] ="BA";
+char pais [20] = "Brasil";
+char estado [20] = "Bahia";    
+char codgcidade [4] ="B01";
 char nome [20] = "Bahia";
 int populacao = 300000;
 float area = 60596.57;
 double pib = 30000.54;
 int turismo = 6;
 
-printf ("codigo da cidade: %s\n", cidade);
+printf ("país: %s\n", pais);
+printf ("estado: %s\n", estado);
+printf ("codigo da cidade: %s\n", codgcidade);
 printf ("nome: %s\n", nome);
 printf ("populacao da cidade: %d\n", populacao);
 printf ("area %.2f\n", area);
